9d720c84e8dcad1d056910ee18c4593daee5ae80
Edward Whelan <vroxing@gmail.com>
2018-12-12 12:52:21 -0500

    basic set up for connection to MLAB

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,5 +8,14 @@ const app = express();
 
 //BodyParser Middleware
 
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
+const db = process.env.MLAB_URI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
